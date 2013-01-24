b6f0d6ef4ec276a51e10561fdcf368fd3348e314
jubianchi <contact@jubianchi.fr>
2013-01-24 01:19:11 +0100

    Update Jenkins git config

--- cookbooks/box/recipes/default.rb
+++ cookbooks/box/recipes/default.rb
@@ -34,6 +34,16 @@ execute "Change jenkins default shell" do
      not_if "cat /etc/passwd | grep jenkins | grep -o bash"
 end
 
+execute "Set jenkins git username" do
+     command "sudo -u jenkins -i git config --global user.name jenkins"
+     not_if "test -f ~jenkins/.gitconfig && cat ~jenkins/.gitconfig | grep name"
+end
+
+execute "Set jenkins git email" do
+     command "sudo -u jenkins -i git config --global user.email jenkins@php-ci.box"
+     not_if "test -f ~jenkins/.gitconfig && cat ~jenkins/.gitconfig | grep email"
+end
+
 template "/etc/motd.tail" do
     source "php.logo"
 end
